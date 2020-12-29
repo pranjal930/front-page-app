0964b2cfab402bc012980fba2307d8ab97195185
pranjal930 <pranjal@rapidken.ai>
2020-12-29 19:41:01 +0530

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,7 @@ import { Meta } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'assignment2';
   constructor(private metaTagService:Meta){}
-  ngOnInit(){
-    this.metaTagService.addTags([
-				{ property:'description' , content : 'hello'},
-				{ property:'og:description' , content : 'hello' },
-				{ property:'og:description' , content : 'hello'},
-				{ property:'og:title' , content : " - Fast learning for the serious learner" },
-				{ property:'title' , content : " - Fast Learning" },
-				{ property:'twitter:card' , content : "summary" },
-				{ property:'twitter:title' , content : " - Fast learning for the serious learner" },
-				{ property:'twitter:description' , content : 'hello' },
-				{ property:'og:url',content:window.location.href},
-				{ name: 'robots', content: 'index, follow'},
-				
-			]);
-  }
 }
