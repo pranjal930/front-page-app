26481060323e9f48e92fda8bd12c49fe288a0bf1
pranjal930 <56031881+pranjal930@users.noreply.github.com>
2020-12-29 18:21:53 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assignment2';
+  constructor(privat metaTagService:Meta)
+  ngOnInit(){
+    this.metaTagService.addTags([
+				{ property:'description' , content : 'hello'},
+				{ property:'og:description' , content : 'hello' },
+				{ property:'og:description' , content : 'hello'},
+				{ property:'og:title' , content : " - Fast learning for the serious learner" },
+				{ property:'title' , content : " - Fast Learning" },
+				{ property:'twitter:card' , content : "summary" },
+				{ property:'twitter:title' , content : " - Fast learning for the serious learner" },
+				{ property:'twitter:description' , content : 'hello' },
+				{ property:'og:url',content:window.location.href},
+				{ name: 'robots', content: 'index, follow'},
+				
+			]);
+  }
 }
